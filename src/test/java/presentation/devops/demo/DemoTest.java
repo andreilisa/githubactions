7a3440f86966d1dca4c8adbdbeb8962e75b704c9
andreilisa <andreilisa2001@gmail.com>
2024-01-20 13:53:07 +0200

    Initial commit

---
+++ src/test/java/presentation/devops/demo/DemoTest.java
@@ -0,0 +1,15 @@
+package presentation.devops.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class DemoTest {
+
+    @Test
+    public void demoTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
