f733db633af8aa2f0baa6224f44c7c1b23515c94
andreilisa <andreilisa2001@gmail.com>
2024-01-28 18:53:58 +0200

    check for cache

--- src/test/java/presentation/devops/demo/DemoTest.java
+++ src/test/java/presentation/devops/demo/DemoTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 
 public class DemoTest {
@@ -11,5 +12,10 @@ public class DemoTest {
     @Test
     public void demoTest() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void demoTest2() {
+        assertNotEquals(2, 1 + 3);
     }
 }
