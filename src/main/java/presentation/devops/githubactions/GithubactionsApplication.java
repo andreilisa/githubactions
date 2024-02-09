61592ab05cdcbd9b7c2ede4d796c10f4789cbcc9
andreilisa <andreilisa2001@gmail.com>
2024-02-09 15:05:12 +0200

    FIX

--- src/main/java/presentation/devops/githubactions/GithubactionsApplication.java
+++ src/main/java/presentation/devops/githubactions/GithubactionsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubactionsApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(GithubactionsApplication.class, args);
     }
